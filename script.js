b066ed32052c17401d2c2508b78dc29ad65f575a
Waribu George <gwaribu@gmail.com>
2022-07-09 01:15:14 +0300

    wording tweaks

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-
 // all of our quotes
 const quotes = [
-    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
-    'There is nothing more deceptive than an obvious fact.',
-    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
-    'I never make exceptions. An exception disproves the rule.',
-    'What one man can invent another can discover.',
-    'Nothing clears up a case so much as stating it to another person.',
-    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+  "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
+  "Actually my hamster is not squeaking to me after he found out I post his little secret. Please apologize to your giraffe on my hamster behalf.",
+  "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
+  "Generated pseudo-randomly using Mathematica’s English library, although edited slightly to make it grammatically correct.",
+  "Well that elephant should attempt some re-realistic broccoli and pies armed wrestling",
+  "Yes how many times should I tell you Holly to eat the black crayons instead of the yellow ones!the black ones are more poisonous!",
+  "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",
+  "The smartness of a pale mango can be uncharacteristically ascertained by the ergonomics of the late nineteenth century bamboo typewriter, whose ribbon has been bitten by a gibbon in heat.",
 ];
 // store the list of words and the index of the word the player is currently typing
 let words = [];
@@ -15,32 +15,34 @@ let wordIndex = 0;
 // the starting time
 let startTime = Date.now();
 // page elements
-const quoteElement = document.getElementById('quote');
-const messageElement = document.getElementById('message');
-const typedValueElement = document.getElementById('typed-value');
+const quoteElement = document.getElementById("quote");
+const messageElement = document.getElementById("message");
+const typedValueElement = document.getElementById("typed-value");
 
-document.getElementById('start').addEventListener('click', () => {
+document.getElementById("start").addEventListener("click", () => {
   // get a quote
   const quoteIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[quoteIndex];
   // Put the quote into an array of words
-  words = quote.split(' ');
+  words = quote.split(" ");
   // reset the word index for tracking
   wordIndex = 0;
 
   // UI updates
   // Create an array of span elements so we can set a class
-  const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+  const spanWords = words.map(function (word) {
+    return `<span>${word} </span>`;
+  });
   // Convert into string and set as innerHTML on quote display
-  quoteElement.innerHTML = spanWords.join('');
+  quoteElement.innerHTML = spanWords.join("");
   // Highlight the first word
-  quoteElement.childNodes[0].className = 'highlight';
+  quoteElement.childNodes[0].className = "highlight";
   // Clear any prior messages
-  messageElement.innerText = '';
+  messageElement.innerText = "";
 
   // Setup the textbox
   // Clear the textbox
-  typedValueElement.value = '';
+  typedValueElement.value = "";
   // set focus
   typedValueElement.focus();
   // set the event handler
@@ -49,7 +51,7 @@ document.getElementById('start').addEventListener('click', () => {
   startTime = new Date().getTime();
 });
 
-typedValueElement.addEventListener('input', () => {
+typedValueElement.addEventListener("input", () => {
   // Get the current word
   const currentWord = words[wordIndex];
   // get the current value
@@ -59,26 +61,28 @@ typedValueElement.addEventListener('input', () => {
     // end of sentence
     // Display success
     const elapsedTime = new Date().getTime() - startTime;
-    const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+    const message = `CONGRATULATIONS! You finished in ${
+      elapsedTime / 1000
+    } seconds.`;
     messageElement.innerText = message;
-  } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+  } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
     // end of word
     // clear the typedValueElement for the new word
-    typedValueElement.value = '';
+    typedValueElement.value = "";
     // move to the next word
     wordIndex++;
     // reset the class name for all elements in quote
     for (const wordElement of quoteElement.childNodes) {
-      wordElement.className = '';
+      wordElement.className = "";
     }
     // highlight the new word
-    quoteElement.childNodes[wordIndex].className = 'highlight';
+    quoteElement.childNodes[wordIndex].className = "highlight";
   } else if (currentWord.startsWith(typedValue)) {
     // currently correct
     // highlight the next word
-    typedValueElement.className = '';
+    typedValueElement.className = "";
   } else {
     // error state
-    typedValueElement.className = 'error';
+    typedValueElement.className = "error";
   }
 });
